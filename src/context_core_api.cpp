f07c5bc571a92bc7d2290f3c9163d79ee69a6e02
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-06-18 20:33:53 +0300

    Make the library call boost code.

--- src/context_core_api.cpp
+++ src/context_core_api.cpp
@@ -1,24 +1,23 @@
 #include "context_core_api.h"
 
-// #include <boost/context/continuation.hpp>
+#include <boost/context/continuation.hpp>
 
-// using ctx = boost::context::detail;
+namespace ctx = boost::context::detail;
 
 context_core_api_fcontext_t
 context_core_api_make_fcontext(void* sp, size_t size, void (*fn)(context_core_api_transfer_t)) {
-  return nullptr;
-  //   return ctx::jump_fcontext(sp, size, fn);
+  return ctx::make_fcontext(sp, size, reinterpret_cast<void (*)(ctx::transfer_t)>(fn));
 }
 
 context_core_api_transfer_t context_core_api_jump_fcontext(context_core_api_fcontext_t const to,
                                                            void* vp) {
-  return {nullptr, nullptr};
-  //   return ctx::jump_fcontext(to, vp);
+  auto r = ctx::jump_fcontext(to, vp);
+  return {r.fctx, r.data};
 }
 
 context_core_api_transfer_t
 context_core_api_ontop_fcontext(context_core_api_fcontext_t const to, void* vp,
                                 context_core_api_transfer_t (*fn)(context_core_api_transfer_t)) {
-  return {nullptr, nullptr};
-  //   return ctx::ontop_fcontext(to, vp, fn);
+  auto r = ctx::ontop_fcontext(to, vp, reinterpret_cast<ctx::transfer_t (*)(ctx::transfer_t)>(fn));
+  return {r.fctx, r.data};
 }
