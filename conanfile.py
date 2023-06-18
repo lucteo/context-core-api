7f0bd97dcf1e0cfd138ec870050023d9787dc813
Lucian Radu Teodorescu <lucteo@lucteo.ro>
2023-06-18 20:21:09 +0300

    Fix test package compilation.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 
 class ContextCoreApiConan(ConanFile):
-    name = "context-core-api"
+    name = "context_core_api"
     version = "1.0.0"
 
     # Optional metadata
@@ -44,13 +44,17 @@ class ContextCoreApiConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["context-core-api"]
+        self.cpp_info.libs = ["context_core_api"]
 
     # cpp_info.components["mycomp"].includedirs
     # cpp_info.components["mycomp"].libdirs
 
 # from <root>/build/ directory, run:
-#   > conan install .. --build=missing -s cppstd=20
+#   > conan install .. --build=missing
 #
 # then:
 #   > conan build ..
+#
+# publish and test the package with:
+#   > conan export ..
+#   > conan test ../test_package context_core_api/1.0.0 --build=missing
